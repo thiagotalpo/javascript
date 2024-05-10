e952c50a98b32032af7ece11047b410d341127e4
Thiago Talpo <166398726+thiagotalpo@users.noreply.github.com>
2024-05-10 10:17:31 -0300

    att

--- Exercícios JavaScript/desafio003/script.js
+++ Exercícios JavaScript/desafio003/script.js
@@ -4,13 +4,17 @@ function contar(){
     let passo = document.getElementById('passoi')
     let res = document.getElementById('res')
 
-    if (passo.value == 0 || inicio.value == 0 || fim.value == 0){
+    if (passo.value == 0 || inicio.value == 0 || fim.value.lenght == 0){
         window.alert('ERRO digite um valor válido')
     }else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido ! Passo = 1')
+            p = 1
+        }
         if (i < f){
             for (c = i; c <= f; c += p){
                 res.innerHTML += `${c} \u{1F449} `
